b78606dd71a35426c15f820aee7dda0d7799c751
edte <zzzzip6@gmail.com>
2025-04-29 16:50:17 +0800

    init project

---
+++ README.md
@@ -0,0 +1,25 @@
+# lsp-hover.nvim
+show lsp reference count and last git commiter
+
+![before](https://github.com/user-attachments/assets/e917728e-ac40-49a9-9ddf-2e1213ac25de)
+
+![after](https://github.com/user-attachments/assets/b9748457-daad-481f-9818-052688f8b909)
+
+
+## 📦 Installation
+
+### [lazy.nvim](https://github.com/folke/lazy.nvim)
+
+```lua
+{
+    "edte/lsp-hover.nvim",
+    opts = {},
+},
+```
+
+
+## 🚀 Usage
+`k` to show hover
+
+## 📄 Thanks
+- [OXY2DEV/fancy_lsp_hover](https://gist.github.com/OXY2DEV/645c90df32095a8a397735d0be646452)
---
+++ mini.lua
@@ -0,0 +1,70 @@
+-- mini config
+-- debug
+vim.lsp.config("lua_ls", {
+	name = "lua_ls",
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	on_attach = function(client, buf)
+		vim.lsp.inlay_hint.enable(true, { bufnr = buf })
+	end,
+	single_file_support = true,
+	root_markers = { ".git", "Makefile" },
+
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim", "require" },
+			},
+			runtime = {
+				version = "LuaJIT",
+			},
+			hint = {
+				enable = true, -- necessary
+				arrayIndex = "Enable",
+				await = true,
+				paramName = "All",
+				paramType = true,
+				-- semicolon = "All",
+				setType = true,
+			},
+			workspace = {
+				checkThirdParty = false,
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			completion = {
+				callSnippet = "Replace",
+			},
+		},
+	},
+	handlers = {
+		["textDocument/definition"] = function(err, result, ctx, config)
+			if type(result) == "table" then
+				result = { result[1] }
+			end
+			vim.lsp.handlers["textDocument/definition"](err, result, ctx, config)
+		end,
+	},
+})
+
+vim.lsp.enable("lua_ls")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+		"edte/lsp-hover.nvim",
+		ft = { "lua", "go", "cpp" },
+		opts = {},
+	},
+})
